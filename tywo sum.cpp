7fcae5b1644de8c153eba254d58d35fe3e3cc4c5
Cesarargue <102204913+Cesarargue@users.noreply.github.com>
2022-05-04 09:00:06 -0600

    Add files via upload

---
+++ tywo sum.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int array[9] = {20, 40, 38, 1700, 12, 756, 522, 1982, 2324};
+    for(int a=0; a<9;a++)
+       {
+           cout<<array[a]<<" ,";
+       }
+       cout<<endl;
+        cout<<endl;
+         cout<<endl;
+          cout<<"la suma de los digitos: "<<endl;
+           cout<<array[2] + array[6]<<endl;
+
+
+
+
+       return 0;
+}
